9134193109b41e1bd50ec32d82f4576feb72fa21
Your Name <you@example.com>
2025-07-28 15:20:38 +0000

    4-tasks.py

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module to concurrently spawn task_wait_random n times and return delays."""
+
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn task_wait_random n times with max_delay and return delays."""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = []
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    return delays
