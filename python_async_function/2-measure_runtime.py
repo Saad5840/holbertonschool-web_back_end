d23078b5f1c48fcd10efde59a17131ae09be41c1
Your Name <you@example.com>
2025-07-28 15:18:33 +0000

    2-measure_runtime.py

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -10,7 +10,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """Measure total execution time for wait_n(n, max_delay), return average."""
+    """Measure total execution time for wait_n and return average."""
     start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     end = time.perf_counter()
