e81e67e0de6a475e3c553bd5230b955fca74668a
Your Name <you@example.com>
2025-07-28 14:57:38 +0000

    6, 7, 8,

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""Module that provides a function returning a tuple with a key and squared value."""
+"""Module that provides function returning tuple with key and squared value."""
 
 
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Return a tuple where the first element is k and the second is v squared as a float."""
+    """Return tuple, the first element is k, the second is v squared as float."""
     return (k, float(v ** 2))
