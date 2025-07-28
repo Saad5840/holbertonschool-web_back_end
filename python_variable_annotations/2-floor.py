83261045858e90f95bbd085ec1968ea897291f74
Your Name <you@example.com>
2025-07-28 14:46:19 +0000

    1-concat.py, 2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module that provides a function to get the floor of a floating-point number."""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of the floating-point number n."""
+    return math.floor(n)
