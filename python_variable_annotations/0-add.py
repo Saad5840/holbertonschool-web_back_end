e3710416b54459c35c98261d8edf62181405d6f3
Your Name <you@example.com>
2025-07-28 14:43:44 +0000

    0-add.py

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Module that provides a function to add two floating-point numbers."""
+
+def add(a: float, b: float) -> float:
+    """Return the sum of two floating-point numbers a and b."""
+    return a + b
