646a0383caf6fc0cc5d39446c2c04c4dbf0a28cb
Your Name <you@example.com>
2025-07-28 15:15:20 +0000

    1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Module to concurrently spawn wait_random n times and return sorted delays."""
+"""Module to concurrently spawn wait_random n times and return delays."""
+
 
 import asyncio
 from typing import List
@@ -8,7 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn wait_random n times with max_delay and return sorted list of delays."""
+    """Spawn wait_random n times with max_delay and return delays."""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     delays = []
     for task in asyncio.as_completed(tasks):
