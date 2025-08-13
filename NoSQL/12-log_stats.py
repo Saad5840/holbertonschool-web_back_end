43cc7132ac7a4e5f728a9a4c301844fdd8350eb0
Your Name <you@example.com>
2025-08-13 14:30:09 +0000

    12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,30 +1,26 @@
 #!/usr/bin/env python3
-"""Script that provides some stats about Nginx logs stored in MongoDB"""
-
+"""
+a Python script that provides some stats about Nginx logs stored in MongoDB
+"""
 from pymongo import MongoClient
 
 
-def main():
-    """Fetch and display stats about nginx logs"""
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    nginx = db.nginx
-
-    # Total number of logs
-    total_logs = nginx.count_documents({})
-    print(f"{total_logs} logs")
-
-    # Number of logs per HTTP method
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print("Methods:")
-    for method in methods:
-        count = nginx.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
-
-    # Number of GET /status requests
-    status_count = nginx.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_count} status check")
-
-
 if __name__ == "__main__":
-    main()
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    x = len(list(nginx_collection.find()))
+    print(x, "logs\nMethods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for m in methods:
+        print(
+            "\tmethod {}: {}".format(
+                m, len(list(nginx_collection.find({"method": m})))
+                )
+            )
+    print(
+        "{} status check".format(
+            len(list(
+                nginx_collection.find({"method": "GET", "path": "/status"})
+                ))
+            )
+        )
