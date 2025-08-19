1117b0f5bc60bd0a03beffd1003a6de6e857d209
Your Name <you@example.com>
2025-08-19 17:16:57 +0000

    8-hbtn_class.js

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,33 @@
+// 8-hbtn_class.js
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  // Called when object is cast to a primitive
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+    // default
+    return this._location;
+  }
+}
