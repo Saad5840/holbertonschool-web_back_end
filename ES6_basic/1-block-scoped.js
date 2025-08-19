2c591087dbdaf4111cfcd4712d4bbd9d001d4f3f
Your Name <you@example.com>
2025-08-19 15:43:42 +0000

    1-block-scoped.js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
+// 1-block-scoped.js
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;   // block-scoped
-    const task2 = false; // block-scoped
+    // Do nothing here; don't redeclare or reassign task/task2
   }
 
   return [task, task2];
