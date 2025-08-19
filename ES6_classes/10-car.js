20149e296aaa82a6473535f8d55c9b7061aeaeb4
Your Name <you@example.com>
2025-08-19 17:18:46 +0000

    10-car.js

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,18 @@
+// 10-car.js
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  // Ensure cloneCar returns an instance of the same constructor
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const C = this.constructor[Symbol.species];
+    return new C();
+  }
+}
