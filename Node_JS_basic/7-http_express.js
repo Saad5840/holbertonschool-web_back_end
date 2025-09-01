df7325c1dd2967bb79064149c52df90274fc9ac7
Your Name <you@example.com>
2025-09-01 16:01:34 +0000

    7-http_express.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,74 +1,23 @@
-#!/usr/bin/node
-
 const express = require('express');
-const fs = require('fs');
+const countStudents = require('./3-read_file_async');
 
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
+const port = 1245;
 
-      const lines = data
-        .split('\n')
-        .filter((line) => line.trim() !== '');
-
-      if (lines.length <= 1) {
-        resolve('Number of students: 0');
-        return;
-      }
-
-      // remove header
-      lines.shift();
-
-      const fields = {};
-      let total = 0;
-
-      for (const line of lines) {
-        const parts = line.split(',');
-        if (parts.length < 4) continue;
-
-        const firstname = parts[0].trim();
-        const field = parts[3].trim();
-
-        if (!firstname || !field) continue;
-
-        if (!fields[field]) fields[field] = [];
-        fields[field].push(firstname);
-        total += 1;
-      }
-
-      let output = `Number of students: ${total}`;
-      for (const field of Object.keys(fields)) {
-        output += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
-      }
-
-      resolve(output);
-    });
-  });
-}
-
-const database = process.argv[2];
-const app = express();
-
-app.get('/', (req, res) => {
+const app = express().get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  res.type('text/plain');
   res.write('This is the list of our students\n');
-  countStudents(database)
-    .then((output) => {
-      res.end(output);
+  countStudents(process.argv[2])
+    .then((data) => {
+      res.end(data);
     })
-    .catch(() => {
-      res.end('Cannot load the database');
+    .catch((error) => {
+      res.end(error.message);
     });
 });
 
-app.listen(1245);
+app.listen(port);
 
 module.exports = app;
